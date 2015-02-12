6d60a919c08778947be9f0f35ec1fe5536a02d8c
Héctor Palma Téllez <hector@palmatellez.cl>
2015-02-12 20:57:01 -0300

    FullPage funcionando.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+     $('#animate').fullpage({
+        navigation: true,
+        loopBottom: true,
+        recordHistory: true,
+        sectionsColor : ['red', 'blue', 'green', 'yellow']
+
+     });
+});

a70302ea65cb48384870ac0f5041a4e29cec6bcf
Héctor Palma Téllez <hector@palmatellez.cl>
2015-03-20 18:50:26 -0300

    Fondo en primer slide

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
         navigation: true,
         loopBottom: true,
         recordHistory: true,
-        sectionsColor : ['#F0EEBD', '#F7F1E5', '#F0CEBD'],
+        // sectionsColor : ['#F0EEBD', '#F7F1E5', '#F0CEBD'],
         verticalCentered: false,
         fixedElements: '.navigation',
         anchors: ['presentacion', 'catalogo', 'contacto']
